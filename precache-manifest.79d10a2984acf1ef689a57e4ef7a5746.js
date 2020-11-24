db6df52eabab07990c3352fff4676bde19fad9de
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-11-24 23:52:01 +0100

    Updates

---
+++ precache-manifest.79d10a2984acf1ef689a57e4ef7a5746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a75d5a3298b953162c2bb6ded491fee",
+    "url": "/Form/index.html"
+  },
+  {
+    "revision": "59a2f03afac857104dcf",
+    "url": "/Form/static/css/main.b0c40d7f.chunk.css"
+  },
+  {
+    "revision": "000c594e3aad4842e641",
+    "url": "/Form/static/js/2.23ebce42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Form/static/js/2.23ebce42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a2f03afac857104dcf",
+    "url": "/Form/static/js/main.00b03d7b.chunk.js"
+  },
+  {
+    "revision": "9b1ab879a6fcf1058293",
+    "url": "/Form/static/js/runtime-main.7b89d411.js"
+  }
+]);
